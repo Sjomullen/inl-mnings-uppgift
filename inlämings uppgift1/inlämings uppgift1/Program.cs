3e7925020dcfb244f5db6f7e8ece0dde5081ead2
Sjomullen <filip.sjogren@outlook.com>
2019-09-06 09:44:59 +0200

    la till några rader

---
+++ inlämings uppgift1/inlämings uppgift1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace inlämings_uppgift1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int num = rnd.Next(100);
+            Boolean status = false;
+
+            
+            Console.WriteLine("Guess a number between 1 and 100");
+
+            while( status == false)
+            {
+                Console.WriteLine(Please enter a number: );
+
+                string input = Console.ReadLine();
+                
+
+
+
+            }
+            
+
+
+  
