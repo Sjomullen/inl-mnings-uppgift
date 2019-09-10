61c7b26866ea13e1eb732c3aa92e8f855405dcb8
Sjomullen <filip.sjogren@outlook.com>
2019-09-10 13:38:46 +0200

    gjorde några ändringar i koden

--- inlämings uppgift1/inlämings uppgift1/Program.cs
+++ inlämings uppgift1/inlämings uppgift1/Program.cs
@@ -8,13 +8,13 @@ namespace inlämings_uppgift1
         {
             Random rnd = new Random();
             int compnum = rnd.Next(1, 100);
-            bool status = false;
+            bool status = true;
             int usercount = 0;
 
 
             Console.WriteLine("Guess a number between 1 and 100");
 
-            while (!status)
+            while (status)
             {
                 Console.WriteLine("Please enter a number: ");
                 usercount++;
@@ -25,7 +25,7 @@ namespace inlämings_uppgift1
                 {
                     Console.WriteLine("Congrats you got the right number");
                     Console.WriteLine("You did it on: " + usercount + " tries");
-                    status = true;
+                    status = false;
                 }
                 else
                 {
